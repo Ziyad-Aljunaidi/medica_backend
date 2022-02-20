ffb6ff5e145aaf64c6ae0820c9f21804ed5f5590
Ziyad-Aljunaidi <ziyad.al.junaidi@gmail.com>
2022-02-20 02:45:35 +0200

    added city & speciality result filter

--- app.js
+++ app.js
@@ -8,31 +8,37 @@ const database_manipulation = require("./database_manipulation.js")
 // using the PORT in the .env file
 const port = process.env.PORT;
 
-function searchQuery(urlQuery, data){
+async function searchQuery(urlQuery, data){
   try{
-    if(urlQuery.length != 0){
-      if(urlQuery.city){
-        console.log(urlQuery.city)
-         data = database_manipulation.findListings("city", urlQuery.city);
-         return data;
-        
-      } 
-      else if(urlQuery.speciality){
-        console.log(urlQuery.speciality)
-        data =  database_manipulation.findListings("speciality", urlQuery.speciality)
-          return data;
-        
-      } else if (urlQuery.id){
-        console.log(urlQuery.id)
-        data =  database_manipulation.findListings("id", urlQuery.id);
-        return data;
-      }
+    if(urlQuery.city != undefined && urlQuery.speciality != undefined){
+      console.log("city & speciality queries")
       
+      data = database_manipulation.findListings("city", urlQuery.city);
+      let new_data = await data;
+      console.log(new_data[0].speciality)
+      data = [];
+      for(let i = 0; i<new_data.length; i++){
+        if(new_data[i].speciality === urlQuery.speciality){
+          data.push(new_data[i]);
+          //console.log(new_data[i])
+        }
+      }
     }
-    else{
-      console.log("no url queries found.")
+    else if(urlQuery.city != undefined){
+      console.log(urlQuery.city + " CITY");
+      data = database_manipulation.findListings("city", urlQuery.city);
+    } 
+    else if(urlQuery.speciality != undefined){
+      console.log(urlQuery.speciality+" SPECIALITY")
+      data =  database_manipulation.findListings("speciality", urlQuery.speciality)
+      //  return data;
+      
+    } else if(urlQuery.id != undefined){
+      console.log(urlQuery.id+" ID")
+      data =  database_manipulation.findListings("id", urlQuery.id);
+      // return data;
     }
-    
+    return data;
   }catch(e){
     console.error(e);
   }
@@ -44,9 +50,16 @@ app.get("/", (req, res) =>{
 
 app.get("/api/doctor", (req,res) => {
   let urlQuery = req.query;
-  searchQuery(urlQuery).then((result) =>{
-    res.json(result);
-  });
+  console.log(urlQuery.city, urlQuery.id, urlQuery.speciality)
+  if(Object.keys(urlQuery).length != 0){
+    searchQuery(urlQuery).then((result) =>{
+      res.json(result);
+    });
+    
+  }else{
+    console.log("NO QUERY FOUND!")
+  }
+
 });
 
 
