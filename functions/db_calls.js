13333997ef209d9c0b435a51126925ee45beeef9
Ziyad-Aljunaidi <ziyad.al.junaidi@gmail.com>
2022-04-04 16:27:05 +0200

    db calls doctor dashboard

--- functions/db_calls.js
+++ functions/db_calls.js
@@ -1,5 +1,4 @@
-
- //Initialize Cloud Firestore through Firebase
+//Initialize Cloud Firestore through Firebase
 //import { initializeApp } from "firebase/app";
 //import { getFirestore } from "firebase/firestore";
 //const firebaseApp = initializeApp({
@@ -8,10 +7,11 @@
 //  projectId: "medica72-5933c",
 //});
 //import { collection, getDocs } from "firebase/firestore";
-
-const fbInitApp = require("firebase/app")
-const getFr = require("firebase/firestore")
-
+const schedule = require("node-schedule");
+const { use } = require("express/lib/application");
+const { get } = require("express/lib/request");
+const fbInitApp = require("firebase/app");
+const getFr = require("firebase/firestore");
 
 const firebaseApp = fbInitApp.initializeApp({
   apiKey: "AIzaSyD18CvQu6AWzU9CFu_75aL92nOThgeZd6A",
@@ -19,15 +19,13 @@ const firebaseApp = fbInitApp.initializeApp({
   projectId: "medica72-5933c",
 });
 
-
-
 const db = getFr.getFirestore();
 
-//import res from "express/lib/response";
+let dateObj = Date.now();
 
 async function queryDoctor(doctorId) {
   let resultData = "tezk 7mra";
-  const queryDoc = await  getFr.getDocs(getFr.collection(db, "doctors"));
+  const queryDoc = await getFr.getDocs(getFr.collection(db, "doctors"));
 
   queryDoc.forEach((doc) => {
     if (doc.data()["id"] == doctorId) {
@@ -36,7 +34,6 @@ async function queryDoctor(doctorId) {
   });
   return resultData;
 }
-
 
 async function queryUser(userId) {
   let resultData = "tezk 7mra";
@@ -53,44 +50,104 @@ async function queryUser(userId) {
 
 async function querySpeciality(docSpeciality) {
   const query_user = await getFr.getDocs(getFr.collection(db, "doctors"));
-  let resultData =[];
+  let resultData = [];
   query_user.forEach((doc) => {
     if (doc.data()["category"] == docSpeciality) {
       resultData.push(doc.data());
     }
-
   });
   return resultData;
 }
 
+async function getAppointments(doc_id) {
+  const docRef = getFr.doc(db, "appointments", doc_id);
+  const docSnap = await getFr.getDoc(docRef);
+
+  if (docSnap.exists()) {
+    //console.log("Document data:", docSnap.data());
+    return docSnap.data();
+  } else {
+    // doc.data() will be undefined in this case
+    console.log("No such document!");
+  }
+}
+// getAppointments("17002").then((result) => {
+//   console.log(result)
+// })
+
+async function setAppointments(doc_id, user_id, user_time, status_code) {
+  let patients_obj = {
+    user_id: user_id,
+    user_time: user_time,
+    status_code: status_code,
+    date_stamp: dateObj,
+  };
+
+  try {
+    await getFr.updateDoc(getFr.doc(db, "appointments", doc_id), {
+      patients: getFr.arrayUnion(patients_obj),
+    });
+  } catch (err) {
+    console.log(err);
+    await getFr.setDoc(getFr.doc(db, "appointments", doc_id), {
+      patients: getFr.arrayUnion(patients_obj),
+    });
+  }
+}
+// setAppointments("17006", "66666", "6:69","1");
+
+async function appointmentsToHistory(doc_id) {
+  const docRef = getFr.doc(db, "appointments", doc_id);
+  const docSnap = await getFr.getDoc(docRef);
+  let documentData = docSnap.data().patients;
+
+  let day_date = Date.now();
+  let patients_list = {
+    date: day_date,
+    patients: documentData,
+  };
+
+  console.log(patients_list);
+  try {
+    await getFr.updateDoc(getFr.doc(db, "history_appointments", doc_id), {
+      history: getFr.arrayUnion(patients_list),
+    });
+  } catch (err) {
+    //console.log(err);
+    await getFr.setDoc(getFr.doc(db, "history_appointments", doc_id), {
+      history: getFr.arrayUnion(patients_list),
+    });
+  } finally {
+    await getFr.deleteDoc(getFr.doc(db, "appointments", doc_id));
+  }
+}
+//appointmentsToHistory("17005");
 
 async function AddUsersData(users_db) {
-    try {
-      const docRef = await getFr.addDoc(getFr.collection(db, "users"),users_db);
-      console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-
+  try {
+    const docRef = await getFr.addDoc(getFr.collection(db, "users"), users_db);
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
 }
 
-
-async function SignIn(user_data){
+async function SignIn(user_data) {
   const query_user = await getFr.getDocs(getFr.collection(db, "users"));
-  let resultData =[];
+  let resultData = [];
   query_user.forEach((doc) => {
-    if (doc.data()["email"] == user_data.email && doc.data()["password"] == user_data.password) {
+    if (
+      doc.data()["email"] == user_data.email &&
+      doc.data()["password"] == user_data.password
+    ) {
       resultData.push(doc.data());
     }
-
   });
   return resultData;
 }
 
-//console.log("mawaa")
-
 //  SignIn({email:"zadj99965@gmail.com", password: "cygaMaw"}).then((result)=>{
-  //  console.log(result)
+//  console.log(result)
 //  })
 
 //AddUsersData({id:"456",name:"test"})
@@ -115,10 +172,13 @@ async function SignIn(user_data){
 //}
 // export { queryDoctor, queryUser};
 
-module.exports= {
+module.exports = {
   queryDoctor,
   queryUser,
   querySpeciality,
   AddUsersData,
-  SignIn
-}
+  SignIn,
+  getAppointments,
+  setAppointments,
+  appointmentsToHistory,
+};
